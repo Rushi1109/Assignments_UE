08389a2386a9fd8e0e44f2ef8cb42a5436e83223
Rushi Gandhi <rushigandhi1109@gmail.com>
2024-05-10 16:28:39 +0530

    Create plugin for Assignment4

---
+++ Plugins/Assignment4/Source/Assignment4/Public/DataAsset/MeshAssetManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "MeshAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASSIGNMENT4_API UMeshAssetManager : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
