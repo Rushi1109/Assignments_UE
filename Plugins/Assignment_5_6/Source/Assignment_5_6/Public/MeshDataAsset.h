5f72942213ac8d40acc5ac25b2eff617309e0cf1
Rushi Gandhi <rushigandhi1109@gmail.com>
2024-06-06 11:55:40 +0530

    Create ToonShade Material, DataAsset and Widget

---
+++ Plugins/Assignment_5_6/Source/Assignment_5_6/Public/MeshDataAsset.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "MeshDataAsset.generated.h"
+
+USTRUCT(BlueprintAble)
+struct FMeshProps {
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Mesh Data")
+	UStaticMesh* StaticMesh;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Mesh Data")
+	float MinScale;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Mesh Data")
+	float MaxScale;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Mesh Data")
+	FRotator MinRotation;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Mesh Data")
+	FRotator MaxRotation;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Mesh Data")
+	float MinDistanceBetweenMeshes;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Mesh Data")
+	float MaxDistanceBetweenMeshes;
+};
+
+/**
+ *
+ */
+UCLASS()
+class ASSIGNMENT_5_6_API UMeshDataAsset : public UDataAsset {
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TArray<FMeshProps> MeshData;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TArray<UMaterial*> Materials;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TArray<FLinearColor> Colors;
+};
